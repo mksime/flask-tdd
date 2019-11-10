2ba949195fa4772f0d8467fc77f95b221b94b182
marcelosime <marcelo.sime@gmail.com>
2019-11-10 20:33:42 -0300

    adicionando db

--- app.test.py
+++ app.test.py
@@ -1,13 +1,16 @@
+import os
 import unittest
-
 from app import app
 
 class BasicTestCase(unittest.TestCase):
     def test_index(self):
         tester = app.test_client(self)
         response = tester.get('/', content_type = 'html/text')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b'Hello, World!')
+        self.assertEqual(response.status_code, 404)
+        
+    def test_database(self):
+        tester = os.path.exists('flaskr.db')
+        self.assertTrue(tester)
 
 if __name__ == '__main__':
     unittest.main()
