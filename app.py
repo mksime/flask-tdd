671b1f29e611853fed582c192412ea799d870012
marcelosime <marcelo.sime@gmail.com>
2020-02-01 14:14:21 -0300

    teste git

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, jsonify
 
+
 DATABASE = 'flaskr.db'
 DEBUG = True
 SECRET_KEY = 'my_precious'
